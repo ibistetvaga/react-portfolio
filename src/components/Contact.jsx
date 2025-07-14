934be238289c2f684aacc17e7e0c338e7a0927ee
ibisetvaga <ibisthetraveler@gmail.com>
2025-07-14 13:30:22 -0400

    Animaciones añadidas a través de todo el programa utilizando Framer Motion.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/About.jsx
    	modified:   src/components/Contact.jsx
    	modified:   src/components/Experience.jsx
    	modified:   src/components/Hero.jsx
    	modified:   src/components/Projects.jsx
    	modified:   src/components/Technologies.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,23 @@
 import { CONTACT } from "../constants"
+import { motion } from "framer-motion"
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
-        <div className="text-center  tracking-tighter">
+        <motion.h2 
+        whileInView={{ opacity: 1, x: 0, y: 0 }}
+          initial={{ opacity: 0, y: -100 }}
+          transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl">Get in Touch</motion.h2>
+        <motion.div 
+        whileInView={{ opacity: 1, x: 0, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 0.5 }}
+        className="text-center  tracking-tighter">
             <p className="my-4">{CONTACT.address}</p>
             <p className="my-4">{CONTACT.phoneNo}</p>
             <a href="#" className="border-b">{CONTACT.email}</a>
-        </div>
+        </motion.div>
     </div>
   )
 }
